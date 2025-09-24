cecb13aa7303be5957a91454002a4fc8e483b7fe
dionions <d.santamaria11217@gmail.com>
2025-09-24 16:33:07 -0400

    David's color
    adds additional string if the color matches my brother's favorite color

--- Test.py
+++ Test.py
@@ -1,2 +1,6 @@
 choice = input("What's your favorite color? ")
-print(f"Your favorite color is {choice}")
+if choice == "green":
+    print(f"Your favorite color is {choice}")
+    print(f"This is my brother David's favorite color, too!")
+else:
+    print(f"Your favorite color is {choice}")
