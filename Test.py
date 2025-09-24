3bc0f0545c9f7176adcd64240aff8b03e6970bcf
dionions <d.santamaria11217@gmail.com>
2025-09-24 16:37:31 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,12 +1,9 @@
 choice = input("What's your favorite color? ")
-<<<<<<< HEAD
 if choice == "green":
     print(f"Your favorite color is {choice}")
     print(f"This is my brother David's favorite color, too!")
-=======
-if choice == "red":
+elif choice == "red":
     print(f"Your favorite color is {choice}")
     print("This is my favorite color, too!")
->>>>>>> dion-branch
 else:
     print(f"Your favorite color is {choice}")
